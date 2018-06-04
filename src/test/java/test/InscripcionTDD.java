be6f3972c850161f8e4f20178d946d5c0d442154
eojp <eo-gp@hotmail.com>
2018-06-03 22:06:04 -0300

    Add archivos faltantes en commit inicial

--- src/test/java/test/InscripcionTDD.java
+++ src/test/java/test/InscripcionTDD.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class PackmanTDD {
+public class InscripcionTDD {
 
     @Test
     public void consultaUsuarioPorLegajo(){
         UsuarioBD usuarioBD=new UsuarioBD();
+        usuarioBD.insertar(new Usuario(null, "Omar", "Gomez","123"));
         Usuario usuario=usuarioBD.buscarPorLegajo("123");
         assertEquals("123", usuario.getLegajo());
 
@@ -21,4 +22,4 @@ public class PackmanTDD {
     }
 
 
-}
+
