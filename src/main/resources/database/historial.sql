a0422c569927ca9c6c3c6d90baf54a5cc092589d
Omar <x64x64x64@gmail.com>
2018-07-02 19:52:14 -0300

    Correcion en historial.sql.

--- src/main/resources/database/historial.sql
+++ src/main/resources/database/historial.sql
@@ -5,7 +5,10 @@ CREATE TABLE `historial` (
   `alumno_id` int(11) NOT NULL,
   `cursada_id` int(11) NOT NULL,
   `promedio` int(11) NOT NULL,
-  `fecha` varchar(45) NOT NULL
+  `fecha` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
+
+
 
 
